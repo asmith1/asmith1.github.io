90d61c50d0f4917b7d4d4bbd459c36058820e023
Ashley Smith <ashleysmith@tufts.edu>
2020-12-09 23:52:44 -0500

    opens random card from 2 options

--- javascript/modal-script.js
+++ javascript/modal-script.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("card");
 
 // Get the button that opens the modal
 var btn = document.getElementById("draw-card-button");
@@ -9,6 +9,8 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
+  var card =  cards[Math.floor(Math.random() * cards.length)];
+  document.getElementById("card-text").innerHTML = card;
   modal.style.display = "block";
 }
 
@@ -23,3 +25,12 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var cards = [
+  "Why are dreidels traditional for Chanukah? <br> a) It's fun to spin them <br>  b) Jews played with tops to make the Greek soldiers <br> think they weren't studying Torah<br>c) They get you chocolate or money!<br>d) Without them, menorahs would be lonely : ( <br> Answer: b",
+  "What is a dreidel? <br> a) a UFO <br> b) a kind of dog <br> c) something delicious in my grandma's soup<br>d) it's a top that has four Hebrew letters on it <br> Answer: d"
+]
+
+// var cards = ["hello", "hello1", "2", "red bubble gum"]
+
+var card =  cards[Math.floor(Math.random() * cards.length)];
