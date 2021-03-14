01c4adc921fef811c9eed67ccb8e6cf78d66283f
Ashley Smith <ashleysmith@tufts.edu>
2021-03-13 21:56:07 -0500

    added passover game html, & deck using template from hannukah game

---
+++ javascript/passover-deck.js
@@ -0,0 +1,161 @@
+var deck1 = [
+    {
+      "question": "How many foods are on the Seder plate?<br> <br> a) none <br> b) 3 <br> c) 5 or 6<br> d) 10",
+      "answer": "Answer: : c - egg, shank bone, parsley/greens, horseradish, charoset, and some add romaine lettuce - variations: a beet instead of a shankbone, an orange for women's right to become a rabbi"
+    },
+    {
+      "question": "Which question is NOT part of the Four Questions? <br> <br> a) Why on this night do we not eat bread and only eat matzah <br> b) Why do we dip twice?<br> c) Is it time for dinner yet?<br> d) Why do we all recline at the table?",
+      "answer": "Answer: c"
+    },
+    {
+      "question": "Who reads the Four Questions?<br> <br> a) the middle child<br> b) the youngest child<br> c) the oldest child<br> d) the dog",
+      "answer": "Answer: b - It's traditional for the youngest child to read the Four Questions, but sometimes and older sibling does it until the youngest learns it "
+    },
+    {
+      "question": "Why do we eat bitter herbs at the Seder?<br> <br> a) to grow herbs in our stomach<br> b) to amuse ourselves watching people's faces when they eat the horseradish <br> c) to appreciate how good matzah tastes by comparison <br> d) to remind us of our bitter slavery in Egypt",
+      "answer": "Answer: d"
+    },
+    {
+      "question": "Why do we eat matzah on Passover?<br> <br> a) our dough didn't have time to rise when we were leaving Egypt<br> b) it's yummy covered in chocolate!<br> c) we're on a new diet<br> d) my cat likes eating the crumbs ",
+      "answer": "Answer: a"
+    },
+    {
+      "question": "How many plagues where there?<br> <br> a) 9<br> b) 12<br> c) 10<br> d) Too many to count!",
+      "answer": "Answer: c - blood, frogs, lice, wild animals, pestilence, hail, boils, locusts, darkness, and the plague of the first born"
+    },
+    {
+      "question": "What are the 3 symbols of Passover?<br> <br> a) a pyramid, sphinx, and king tut <br> b) Chanel, Polo, and Apple <br> c) baseball, apple pie, and Chevrolet <br> d) shankbone, matzah, and bitter herbs",
+      "answer": "Answer: d"
+    },
+    {
+      "question": "Who was Miriam?<br> <br> a) a random person I saw on the street <br> b) Moses' Cat <br> c) the Pharoah's wife <br> d) Moses' sister ",
+      "answer": "Answer: d"
+    },
+    {
+      "question": "How many times do we traditionally wash our hands at the Seder?<br> <br> a) 1 time <br> b) 2 times <br> c) None- We wash before the seder <br> d) Depends on how messy dinner was ",
+      "answer": "Answer: b - once without a blessing (urchatz) and once with a blessing (rachtza)"
+    },
+    {
+      "question": "Which matzah do we break for the Afikoman?<br> <br> a) the one we feel like breaking <br> b) the middle of the stack of three <br> c) the smallest <br> d) the one my dog won't eat",
+      "answer": "Answer: b - We break the middle matzah, and hide half for the Afikoman"
+    },
+    {
+      "question": " What blessings do we say on matzah?<br> <br> a) Dear G-d, let me not choke on this dry cracker <br> b) ha-motzi lechem min ha'aretz, and al achilat matzah <br> c) la'asok b'divrei torah <br> d) none of the above ",
+      "answer": "Answer: b"
+    },
+    {
+      "question": "What blessings do we say on wine or grape juice?<br> <br> a) shechechiyanu <br> b) ha-motzi <br> c) borei pri hagafen <br> d) barchu",
+      "answer": "Answer: c"
+    },
+    {
+      "question": "What blessings do we say on the candles?<br> <br> a) ha-motzi <br> b) le-hitateif ba-tzizit <br> c) borei pri hagafen <br> d) l'hadlik ner shel yom tov",
+      "answer": "Answer: d"
+    },
+    {
+      "question": "What blessings do we say on the bitter herbs?<br> <br> a) Baruch ata ado-nai bitter herbs<br> b) Al achilat maror <br> c) Thank G-d I survived <br> d) Thank G-d we only eat this at Passover",
+      "answer": "Answer: b"
+    },
+    {
+      "question": "What is a Hillel sandwich?<br> <br> a) Bitter herbs between two pieces of matzah <br> b) A sandwich by a guy named Hillel from the Food Network <br> c) A sandwich you eat at college <br> d) Turkey, Mayonnaise, and Mustard on flatbread",
+      "answer": "Answer: a"
+    },
+    {
+      "question": "Why do we eat hard boiled eggs dipped in salt water?<br> <br> a) Because we can't eat raw eggs dipped in salt water <br> b) To remind us of the holiday offering that was made in the Holy Temple of Jerusalem <br> c) They were easy to pack when we were leaving Egypt <br> d) To represent the Dead Sea",
+      "answer": "Answer: b"
+    },
+    {
+      "question": "What is the last piece of food we eat at the Seder?<br> <br> a) sweet and sour meatballs <br> b) fondue <br> c) the afikoman <br> d) cheese and fruit ",
+      "answer": "Answer: c"
+    },
+    {
+      "question": "Discussion Question: <br><br> If you could add your own personal symbol of freedom to the Seder table, what would you choose?<br><br> (There are no wrong answers.)"
+    },
+    {
+      "question": "Discussion Question:<br><br> Why do you think the rabbis teach us about The Four Children?<br><br> (There are no wrong answers.)"
+    },
+    {
+      "question": "Discussion Question:<br><br>What is your favorite Passover food?<br><br>(There are no wrong answers.)"
+    },
+    {
+      "question": "Discussion Question:<br><br>What is your favorite Passover memory?<br><br>(There are no wrong answers.)"
+    },
+    {
+      "question": "Discussion Question:<br><br>Does your family have any special heirlooms that they use for the Seder? If so, tell us about them.<br><br>(There are no wrong answers.)"
+    },
+    {
+      "question": "What book do we read at the Passover Seder?<br> <br> a) Harry Potter <br> b) Twilight <br> c) The Haggadah <br> d) The Hunger Games ",
+      "answer": "Answer: c"
+    },
+    {
+      "question": "Why do we recline at the Passover table?<br> <br> a) Because we're so tired from cooking dinner <br> b) We're lazy <br> c) Why not?! <br> d) We celebrate being free by reclining like royalty ",
+      "answer": "Answer: d"
+    },
+    {
+      "question": " Where do we wash our hands at the Seder?<br> <br> a) At the Seder table <br> b) In the bathroom <br> c) At the hose outside <br> d) In the kitchen ",
+      "answer": "Answer: a"
+    },
+    {
+      "question": " How many glasses of wine or grape juice do we traditionally drink at the Seder?<br> <br> a) 4 <br> b) 3 <br> c) Not enough <br> d) None ",
+      "answer": "Answer: a - for the 4 ways G-d delivered us from Egypt: I shall take you out, I shall rescue you, I shall redeem you, and I shall bring you (Exodus 6:6-8)"
+    },
+    {
+      "question": " What is the 5th cup of grape juice or wine at the Seder table?<br> <br> a) Mine! <br> b) Decoration <br> c) Extra - in case someone is thirsty <br> d) Elijah's cup ",
+      "answer": "Answer: d - Elijah's cup reminds us to look to the future when the world will be at peace."
+    },
+    {
+      "question": " Who do we celebrate by adding a cup or a pitcher of water to our Seder table?<br> <br> a) Moses <br> b) President Obama <br> c) Miriam <br> d) Ariel from Little Mermaid ",
+      "answer": "Answer: c - We remember Miriam with water because wherever Miriam traveled, a well followed her."
+    },
+    {
+      "question": " What blessing do we say on parsley/greens?<br> <br> a) The Haftarah <br> b) The Torah blessings <br> c) Borei Pri Ha'adamah <br> d) The Barchu ",
+      "answer": "Answer: c - Borei Pri Ha'adamah, blessing the fruit of the earth"
+    },
+    {
+      "question": " What is Miriam's cup?<br> <br> a) The one Miriam left here last year <br> b) A sporting event held during Passover <br> c) The measuring cup she uses for cooking  <br> d) A cup of water because a well followed Miriam wherever she went ",
+      "answer": "Answer: d"
+    },
+    {
+      "question": " Discussion Question:<br><br> Passover celebrates our freedom from slavery in Egypt and the beginning of our journey to a new land. Why do you think Passover is important today?<br><br>(There are no wrong answers.)"
+    },
+    {
+      "question": " Discussion Question:<br><br> The Birkat HaMazon thanks G-d for food. Why should we be thankful for food?<br><br> (There are no wrong answers.)"
+    },
+    {
+      "question": " In which Hebrew month in the Jewish calendar does Passover fall?<br> <br> a) Adar <br> b) Tishrei <br> c) Nisan  <br> d) Hyundai ",
+      "answer": "Answer: c"
+    },
+    {
+      "question": " What is the Afikoman?<br> <br> a) A video game <br> b) A type of Japanese food <br> c) A superhero  <br> d) Half the middle matzah that we hide and search for ",
+      "answer": "Answer: d"
+    },
+    {
+      "question": " How many pieces of matzah do we traditionally put on a Matzah plate at the Seder?<br> <br> a) None - they get eaten too fast <br> b) 3 <br> c) 2 because one is hidden  <br> d) 12 because we put the whole box of matzah on the plate ",
+      "answer": "Answer: b - the 3 pieces of matzah represent: the Kohanim (high priests), the Levites (priests), and Yisrael, the rest of the Jewish people "
+    },
+    {
+      "question": " What item do we NOT find on a Seder plate?<br> <br> a) Shankbone <br> b) Egg <br> c) Gefilte Fish  <br> d) Charoset ",
+      "answer": "Answer: c "
+    },
+    {
+      "question": " What do we dip in salt water?<br> <br> a) Our fingers <br> b) Greens <br> c) Matzah  <br> d) Chocolate ",
+      "answer": "Answer: b - Many people use parsley, but sometimes you see celery or other greens. "
+    },
+    {
+      "question": " When do we eat dinner at the Seder?<br> <br> a) At the beginning <br> b) At the end <br> c) We don't eat dinner at the Seder  <br> d) After we have the Hillel sandwich ",
+      "answer": "Answer: d - We eat dinner after we have eaten all of the ritual foods except the Afikoman, which we eat at the end of dinner. "
+    },
+    {
+      "question": " What is the Hebrew name for Passover?<br> <br> a) Pesach <br> b) Z'man Cheirutanu (time of our freedom) <br> c) Chag Ha'Aviv (holiday of Spring) <br> d) All of the above ",
+      "answer": "Answer: d "
+    },
+    {
+      "question": " Which one is NOT one of the Four Children?<br> <br> a) The Wise Child <br> b) The Simple Child <br> c) The Sneezy Child <br> d) The one who doesn't know  ",
+      "answer": "Answer: c - the 4 children are the wise, the wicked, the simple, and the one who does not know to ask "
+    },
+    {
+      "question": " Discussion Question:<br><br> Do you have a favorite song from the Passover seder? <br> (Bonus: You get an extra turn if you sing it.) <br><br> (There are no wrong answers.)"
+    },
+    {
+      "question": " Discussion Question:<br><br>G-d sent Moses to speak up to Pharoah on behalf of the Israelites. Moses was afraid, but followed G-d's advice and did it anyway. Have you ever had to stand up to somebody? What was the situation, and how did you feel? <br><br> (There are no wrong answers.)"
+    }
+  ];
